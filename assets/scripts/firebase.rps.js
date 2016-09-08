46e7fbd85a833089aa9d97cdeefa9df0cd3cacaa
Andrew Carr <acarr@mail.utexas.edu>
2016-09-08 01:51:12 -0500

    no ignore

---
+++ assets/scripts/firebase.rps.js
@@ -0,0 +1,13 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCGIiQ6BTKB52yLAkdEO7nHX4GD_ETSbr8",
+    authDomain: "rockpaperscissors-c4ea7.firebaseapp.com",
+    databaseURL: "https://rockpaperscissors-c4ea7.firebaseio.com",
+    storageBucket: "",
+  };
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
